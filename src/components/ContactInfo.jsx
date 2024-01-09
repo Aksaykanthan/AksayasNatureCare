5f386d19a0a832bf534b3516b37e5016afcf7446
Aksay <aksaykanthan@gmail.com>
2024-01-09 22:31:28 +0530

    first commit

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function ContactInfo() {
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <h2 className="fs-1 mb-3 text-uppercase fw-bold">Where To Find Us</h2>
+            <p className="mb-5">HIG 345, T.N.H.B.Phase-1, Tirupathur, Tamil Nadu 635601</p>
+            <h3>Opening Hours</h3>
+            <p className="m-0">Tue - Sun: 09:00 - 1:00 3:00-7:00</p>
+        </div>
+    )
+}
